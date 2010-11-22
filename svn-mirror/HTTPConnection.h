0a8fd646930a90ef0bfd325c079d66a1894ca275
Michael Papp <mikekppp@aim.com>
2010-11-21 23:28:42 -0800

    adding live svn mirror of cocoahttpserver library

---
+++ svn-mirror/HTTPConnection.h
@@ -0,0 +1,83 @@
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_IPHONE
+// Note: You may need to add the CFNetwork Framework to your project
+#import <CFNetwork/CFNetwork.h>
+#endif
+
+@class AsyncSocket;
+@class HTTPMessage;
+@class HTTPServer;
+@class WebSocket;
+@protocol HTTPResponse;
+
+
+#define HTTPConnectionDidDieNotification  @"HTTPConnectionDidDie"
+
+@interface HTTPConnection : NSObject
+{
+	AsyncSocket *asyncSocket;
+	HTTPServer *server;
+	
+	HTTPMessage *request;
+	int numHeaderLines;
+	
+	NSString *nonce;
+	long lastNC;
+	
+	NSObject<HTTPResponse> *httpResponse;
+	
+	NSMutableArray *ranges;
+	NSMutableArray *ranges_headers;
+	NSString *ranges_boundry;
+	int rangeIndex;
+	
+	UInt64 requestContentLength;
+	UInt64 requestContentLengthReceived;
+	
+	NSMutableArray *responseDataSizes;
+}
+
+- (id)initWithAsyncSocket:(AsyncSocket *)newSocket forServer:(HTTPServer *)myServer;
+
+- (BOOL)supportsMethod:(NSString *)method atPath:(NSString *)path;
+- (BOOL)expectsRequestBodyFromMethod:(NSString *)method atPath:(NSString *)path;
+
+- (BOOL)isSecureServer;
+- (NSArray *)sslIdentityAndCertificates;
+
+- (BOOL)isPasswordProtected:(NSString *)path;
+- (BOOL)useDigestAccessAuthentication;
+- (NSString *)realm;
+- (NSString *)passwordForUser:(NSString *)username;
+
+- (NSDictionary *)parseParams:(NSString *)query;
+- (NSDictionary *)parseGetParams;
+
+- (NSString *)requestURI;
+
+- (NSArray *)directoryIndexFileNames;
+- (NSString *)filePathForURI:(NSString *)path;
+- (NSObject<HTTPResponse> *)httpResponseForMethod:(NSString *)method URI:(NSString *)path;
+- (WebSocket *)webSocketForURI:(NSString *)path;
+
+- (void)prepareForBodyWithSize:(UInt64)contentLength;
+- (void)processDataChunk:(NSData *)postDataChunk;
+
+- (void)handleVersionNotSupported:(NSString *)version;
+- (void)handleAuthenticationFailed;
+- (void)handleResourceNotFound;
+- (void)handleInvalidRequest:(NSData *)data;
+- (void)handleUnknownMethod:(NSString *)method;
+
+- (NSData *)preprocessResponse:(HTTPMessage *)response;
+- (NSData *)preprocessErrorResponse:(HTTPMessage *)response;
+
+- (BOOL)shouldDie;
+- (void)die;
+
+@end
+
+@interface HTTPConnection (AsynchronousHTTPResponse)
+- (void)responseHasAvailableData;
+@end
---
+++ svn-mirror/HTTPServer.h
@@ -0,0 +1,72 @@
+#import <Foundation/Foundation.h>
+
+@class AsyncSocket;
+@class WebSocket;
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060 // Mac OS X 10.6
+  #define IMPLEMENTED_PROTOCOLS <NSNetServiceDelegate>
+#elif __IPHONE_OS_VERSION_MIN_REQUIRED >= 40000 // iPhone 4.0
+  #define IMPLEMENTED_PROTOCOLS <NSNetServiceDelegate>
+#else
+  #define IMPLEMENTED_PROTOCOLS 
+#endif
+
+
+@interface HTTPServer : NSObject IMPLEMENTED_PROTOCOLS
+{
+	// Underlying asynchronous TCP/IP socket
+	AsyncSocket *asyncSocket;
+	
+	// Standard delegate
+	id delegate;
+	
+	// HTTP server configuration
+	NSURL *documentRoot;
+	Class connectionClass;
+	
+	// NSNetService and related variables
+	NSNetService *netService;
+	NSString *domain;
+	NSString *type;
+	NSString *name;
+	UInt16 port;
+	NSDictionary *txtRecordDictionary;
+	
+	NSMutableArray *connections;
+	NSMutableArray *webSockets;
+}
+
+- (id)delegate;
+- (void)setDelegate:(id)newDelegate;
+
+- (NSURL *)documentRoot;
+- (void)setDocumentRoot:(NSURL *)value;
+
+- (Class)connectionClass;
+- (void)setConnectionClass:(Class)value;
+
+- (NSString *)domain;
+- (void)setDomain:(NSString *)value;
+
+- (NSString *)type;
+- (void)setType:(NSString *)value;
+
+- (NSString *)name;
+- (NSString *)publishedName;
+- (void)setName:(NSString *)value;
+
+- (UInt16)port;
+- (void)setPort:(UInt16)value;
+
+- (NSDictionary *)TXTRecordDictionary;
+- (void)setTXTRecordDictionary:(NSDictionary *)dict;
+
+- (BOOL)start:(NSError **)errPtr;
+- (BOOL)stop;
+
+- (void)addWebSocket:(WebSocket *)ws;
+
+- (NSUInteger)numberOfHTTPConnections;
+- (NSUInteger)numberOfWebSocketConnections;
+
+@end
